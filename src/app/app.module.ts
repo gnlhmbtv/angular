d24919b571dffac7a8abaca73e23802b6c8b1708
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-02-17 02:37:47 +0400

    feat: a basic project setup using bootstrap for styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
