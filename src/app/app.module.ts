a6df5164928b9035b44c60c3e4d79d4a12bf7244
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-02-18 02:44:02 +0400

    feat: string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import {WarningAlertComponent} from "./warning-alert/warning-alert.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule
