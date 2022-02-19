a6cf87e338010a240f73fd7905e8222b5300ffc2
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-02-19 23:01:58 +0400

    feat: prj-1 : using the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { ServersComponent } from './servers/servers.component';
 import {FormsModule} from "@angular/forms";
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
+// import { RecipeListComponent } from '.recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
@@ -20,11 +21,12 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ServersComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent,
+    // RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
